b8f957b62ec6ff53542d5e3260cc636abe069cda
Kipngeno <alphonce.kipngeno@student.moringaschool.com>
2021-09-04 11:16:50 +0300

    Write a function

--- js/akan.js
+++ js/akan.js
@@ -1,3 +1,5 @@
+function myFunction(){
 // names arrays
 let akanFemaleNames=["Akosua","Adwoa","Abenaa","Akua","Yaa","Afua","Ama"]
 let akanMaleNames=["Kwasi","Kwadwo","Kwabena","Kwaku","Yaw","Kofi","Kwame"]
+};
