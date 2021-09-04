4e519b8c7cd91c19efb5c960e6783e54e373639f
Kipngeno <alphonce.kipngeno@student.moringaschool.com>
2021-09-04 11:45:21 +0300

    Declare variables to store user information

--- js/akan.js
+++ js/akan.js
@@ -2,4 +2,8 @@ function myFunction(){
 // names arrays
 let akanFemaleNames=["Akosua","Adwoa","Abenaa","Akua","Yaa","Afua","Ama"]
 let akanMaleNames=["Kwasi","Kwadwo","Kwabena","Kwaku","Yaw","Kofi","Kwame"]
+
+//user information
+let birthDay=document.getElementById("inputBirthDay").value;
+let gender=document.getElementsByClassName("gender");
 };
