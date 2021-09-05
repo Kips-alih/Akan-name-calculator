38ba3181c9595a772252c814c3eabdba0da5f7c6
Kipngeno <alphonce.kipngeno@student.moringaschool.com>
2021-09-05 06:55:40 +0300

    Change a variable, create control flow to display akan female names

--- js/akan.js
+++ js/akan.js
@@ -5,9 +5,14 @@ let akanMaleNames=["Kwasi","Kwadwo","Kwabena","Kwaku","Yaw","Kofi","Kwame"]
 
 //user information
 let birthDay=document.getElementById("inputBirthDay").value;
-let gender=document.getElementsById("mySelect").value;
+let femaleGender=document.getElementById("mySelect").selectedOptions[0].value;
 
 //manipulate user info
-let text=new Date(birthDay);
-let date=text.getDay();
+let date=new Date(birthDay);
+let yourDay=date.getDay();
+//control flow
+if ( birthDay !=""&&femaleGender) {
+  document.getElementById("akanName").innerHTML ="Your akan name is " +akanFemaleNames[yourDay]+" .";
+  alert("Your akan name is " +akanFemaleNames[yourDay]+" .");
+}
 };
