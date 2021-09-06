2979210653396ff390803dd32a8212918b8d03d2
Kipngeno <alphonce.kipngeno@student.moringaschool.com>
2021-09-06 23:45:44 +0300

    Add a weekday array and modify the js file

--- js/akan.js
+++ js/akan.js
@@ -1,25 +1,31 @@
-function myFunction(){
-// names arrays
-let akanFemaleNames=["Akosua","Adwoa","Abenaa","Akua","Yaa","Afua","Ama"]
-let akanMaleNames=["Kwasi","Kwadwo","Kwabena","Kwaku","Yaw","Kofi","Kwame"]
+  function myFunction(){
+  // names arrays
+  let akanFemaleNames=["Akosua","Adwoa","Abenaa","Akua","Yaa","Afua","Ama"]
+  let akanMaleNames=["Kwasi","Kwadwo","Kwabena","Kwaku","Yaw","Kofi","Kwame"]
+  let weekDays = ["Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday"];
 
-//user information
-let birthDay=document.getElementById("inputBirthDay").value;
-let femaleGender=document.getElementById("mySelect").selectedOptions[0].value;
 
-//manipulate user info
-let date=new Date(birthDay);
-let yourDay=date.getDay();
-//control flow
-if ( birthDay !=""&&femaleGender) {
-  document.getElementById("akanName").innerHTML ="Your akan name is " +akanFemaleNames[yourDay]+" .";
-  alert("Your akan name is " +akanFemaleNames[yourDay]+" .");
-}
-else if(birthDay !=""&&!femaleGender){
-    alert("Your akan name is " +akanMaleNames[yourDay]+" .");
+  //user information
+  let birthDay=document.getElementById("inputBirthDay").value;
+  let femaleGender=document.getElementById("female").value;
+  let maleGender=document.getElementById("male").value;
 
-}
-else{
-  alert("select all the fields")
-}
-};
+  //manipulate user info
+  let date=new Date(birthDay);
+  let yourDay=date.getDay();
+  //control flow
+  if ( birthDay !=""&&femaleGender) {
+
+    document.getElementById("akanName").innerHTML =" You were born on "+ weekDays[yourDay]+ " and your akan name is " +akanFemaleNames[yourDay]+" .";
+    alert(" You were born on "+ weekDays[yourDay]+ " and your akan name is " +akanFemaleNames[yourDay]+" .");
+    }
+    else if(birthDay !=""&&maleGender){
+
+      document.getElementById("akanName").innerHTML =" You were born on "+ weekDays[yourDay]+ " and your akan name is " +akanMaleNames[yourDay]+" .";
+      alert(" You were born on "+ weekDays[yourDay]+ " and your akan name is " +akanMaleNames[yourDay]+" .");
+
+    }
+    else{
+    alert("select all the fields")
+    }
+  };
